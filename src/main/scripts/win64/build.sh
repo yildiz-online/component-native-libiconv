9cd19f636915f95a49f2012e3f147b556a3eac1e
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-12-07 15:28:50 +0100

    [WIP] Make.

--- src/main/scripts/win64/build.sh
+++ src/main/scripts/win64/build.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 
 cd ../../c++
-./configure --disable-shared --enable-static --target=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --prefix=/src/target/classes/win64/
+./configure --disable-shared --enable-static --target=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --prefix=/src/target/classes/win64
 make
-
+make install
 r1=$?
 
 cd /src/src/main/c++/lib/.libs
 ls -l
 
-cd /src/target/classes/win64/
-ls -l
+
 
 mkdir -p /src/target/classes/win64/lib
 mkdir -p /src/target/classes/win64/include
+
+cd /src/target/classes/win64/lib
+ls -l
+
+cd /src/target/classes/win64/include
+ls -l
 
 exit ${r1}
